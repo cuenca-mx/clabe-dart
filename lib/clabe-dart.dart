4bcebd4449ca9d756d9d1401bf262ff32a145715
Jose Gregorio Castro Lazo <josegregorio994@gmail.com>
2019-01-14 10:57:52 -0600

    Add Slack Notifications in travis file (#5)
    * Add Slack Notifications in travis file
    
    * Change Author and remove comments
    
    * Update README with travis info
    
    * Update README with coveralls info
    
    * Add files to coveralls support
    
    * Add files to coveralls
    
    * Update readme
    
    * support for code coverage

--- lib/clabe-dart.dart
+++ lib/clabe-dart.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library clabe_dart;
 
 export 'src/clabe_validation.dart';
-
-// TODO: Export any libraries intended for clients of this package.
